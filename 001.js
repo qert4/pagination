83479fae99010981a8171fbd875f411d2c236773
조세준 <sejunc0301@gmail.com>
2023-07-04 15:49:29 +0900

    first

--- 001.js
+++ 001.js
@@ -28,6 +28,48 @@ function printContacts(contacts){
         $tbody.append(html);    //tbody 안에 화면에서 볼수있도록 html에 담겨있는 데이터들을 추가 해주었다
     }
 }
-function getPagination(){
+function getPagination({pageno ,pagesize, totalcount, blockSize=5}){
+ 
+    const countOfpage = Math.ceil(totalcount/pagesize);
+    const prev= Math.floor((pageno-1)/blockSize) * blockSize;
+        const start = prev + 1;
+        let end = prev + blockSize;
+        let next = end+1; 
+        if(end>=countOfpage){
+            end=countOfpage;
+            next = 0;
+        }
+        return{prev ,start,  end,  next, pageno};  
+}
 
+// 구조분해할당
+function printPagination({prev ,start,  end,  next, pageno}){
+    const $p = $('#pagination')
+    if(prev>0){
+        const html =`
+            <li class="page-item">
+                <a class="page-ltem" href="list.html?pageno=${prev}">이전으로</a>
+            </li>    
+        `;
+        $p.append(html);
+    }
+    for(let i=start; i<=end; i++){
+        let className = 'page-item';
+        if(i===pageno)
+            className = 'page-item active';
+        const html=`
+            <li class="${className}">
+                <a class="page-link" href="list.html?pageno=${i}">${i}</a>s
+            </li>    
+        `;
+        $p.append(html);
+    }
+    if(next>0){
+        const html =`
+        <li class="page-item">
+            <a class="page-link" href="list.html?pageno=${next}">다음으로</a>
+        </li>    
+    `;
+    $p.append(html);
+    }
 }
