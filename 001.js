8b92ee82bf27a7ef6eb8df7fc91aa8074b9b5aec
조세준 <sejunc0301@gmail.com>
2023-07-04 11:43:46 +0900

    수정

--- 001.js
+++ 001.js
@@ -14,7 +14,7 @@ async function fetch(pageno, pagesize=10){
         return null;            // 에러가 발생하면 null 값을 가져오라는 의미이다
     }
 }
-function getContacts(contacts){
+function printContacts(contacts){
     const $tbody = $('#tbody');  //tbody안에 내용들을 넣을 수있는  방을 만든셈이다
     for(const c of contacts){   // contacts안에 있는 데이터들을 하나씩 차례대로 받아와서 const c 안에 넣는다 (contacts안에는 no,name,address,tel 의 내용들이 담겨있다)
         const html = `
@@ -26,8 +26,7 @@ function getContacts(contacts){
             </tr>
         `;
         $tbody.append(html);    //tbody 안에 화면에서 볼수있도록 html에 담겨있는 데이터들을 추가 해주었다
-    };
-   
+    }
 }
 function getPagination(){
 
